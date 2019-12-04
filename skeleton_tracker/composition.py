0e6f77c24b56707d16cb48135fc031cbb412a97f
Andrew <andrewxie@dpengineering.org>
2019-12-04 12:31:27 -0800

    Moar skele

--- skeleton_tracker/composition.py
+++ skeleton_tracker/composition.py
@@ -3,6 +3,10 @@ from .config import global_config as c
 from . import logger
 import os
 
+from . import Skeleton
+sk = Skeleton()
+
+
 l = logger.getChild("composition")
 
 class Kinect:
@@ -16,4 +20,8 @@ class Kinect:
         self.tracker = Tracker(self.tracker_path, self.tracker_wd)
         l.info("Starting main loop")
         for user in self.tracker.stream():
-            print(user)
+            if (user is not None):
+                head, neck, torso, lshoulder, lelbow, lhand, rshoulder, relbow, rhand, lhip, lknee, lfoot, rhip, rknee, rfoot = user
+
+                sk.updateSk(head, neck, torso, lshoulder, lelbow, lhand, rshoulder, relbow, rhand, lhip, lknee, lfoot, rhip, rknee, rfoot)
+                sk.drawSkel()
